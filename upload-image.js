1531797ff6994368940ce0d4aa4ad9ff45138576
Stephen Bannasch <stephen.bannasch@gmail.com>
2021-08-25 17:23:56 -0400

    upload-image: send Blob with unique filename

--- upload-image.js
+++ upload-image.js
@@ -14,24 +14,60 @@ function initializeCanvas(c) {
   ctx.fillRect(30, 30, 50, 50);
 }
 
+let UUID;
+
+UUID = (function () {
+  var i, lut, self;
+  self = {};
+  lut = [];
+  i = 0;
+  while (i < 256) {
+    lut[i] = (i < 16 ? '0' : '') + i.toString(16);
+    i++;
+  }
+  self.generate = function () {
+    var d0, d1, d2, d3;
+    d0 = Math.random() * 0xffffffff | 0;
+    d1 = Math.random() * 0xffffffff | 0;
+    d2 = Math.random() * 0xffffffff | 0;
+    d3 = Math.random() * 0xffffffff | 0;
+    return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-' + lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-' + lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] + lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
+  };
+  return self;
+})();
+
 let canvas = document.getElementById('canvas');
 initializeCanvas(canvas);
 
 let jpgDataUrl = canvas.toDataURL("image/jpeg", 0.9);
+let jpgBlob = null;
+
+canvas.toBlob((blob) => {
+  jpgBlob = blob;
+}, "image/jpeg", 0.9);
+
+let generateJpgName = () => {
+  let uuid = UUID.generate();
+  return `sample-jpg-${uuid}.jpg`;
+}
 
 let formNoCors = document.getElementById('form-no-cors');
 let formCors = document.getElementById('form-cors');
 
-formNoCors.onsubmit = async (e) => {
-  e.preventDefault();
+let postUrl = 'https://waps.cfa.harvard.edu/microobservatory/own_kiosk/uploads/upload_3.php';
 
+createFormData = () => {
   let email = document.getElementById('email');
+  let imageFilename = generateJpgName();
   let formData = new FormData();
   formData.append("email", email.value);
-  formData.append("data", jpgDataUrl);
+  formData.append("data", jpgBlob, imageFilename);
+  return formData;
+};
 
-  let postUrl = 'https://waps.cfa.harvard.edu/microobservatory/own_kiosk/uploads/upload_3.php';
-
+formNoCors.onsubmit = async (e) => {
+  e.preventDefault();
+  let formData = createFormData();
   fetch(postUrl, {
       method: 'POST',
       mode: 'no-cors',
@@ -45,7 +81,7 @@ formNoCors.onsubmit = async (e) => {
       return data ? JSON.parse(data) : {};
     })
     .then(result => {
-      displayAlert(result);
+      alert(result);
     })
     .catch(error => {
       alert('Error:', error);
@@ -54,13 +90,7 @@ formNoCors.onsubmit = async (e) => {
 
 formCors.onsubmit = async (e) => {
   e.preventDefault();
-
-  let email = document.getElementById('email');
-  let formData = new FormData();
-  formData.append("email", email.value);
-  formData.append("data", jpgDataUrl);
-
-  let postUrl = 'https://waps.cfa.harvard.edu/microobservatory/own_kiosk/uploads/upload_3.php';
+  let formData = createFormData();
 
   fetch(postUrl, {
       method: 'POST',
@@ -70,13 +100,8 @@ formCors.onsubmit = async (e) => {
       body: formData
     })
     .then(response => response.json())
-    .then(result => displayAlert(result))
+    .then(json => alert(JSON.stringify(json)))
     .catch(error => {
       alert('Error:', error);
     });
 };
-
-function displayAlert(result) {
-  alert('message =' + result.message + ' \nkiosk_id = ' + result.kiosk_id + ' \nemail = ' + result.email + ' \nobservation = ' + result.observation + ' \ndatetime_when_user_made_request_at_kiosk = ' + result
-    .datetime_when_user_made_request_at_kiosk + ' \ncredential = ' + result.credential);
-}
